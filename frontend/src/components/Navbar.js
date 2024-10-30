5c2f384f7242523669a2a41ef17fbae194000c99
Pallavi Sastry <89313238+pallavi-sastry@users.noreply.github.com>
2024-10-29 20:01:34 -0400

    added a banner

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,9 +2,16 @@ import React from "react"
 
 export default function Navbar() {
   return (
-    <nav className="header">
+    <>
+    <nav className="nav">
       <img src="/images/gflogowhite.png" className="nav--logo" alt="UNC Global Affairs Logo" />
       <h1 className="header-title">UNC COIL Database</h1>
-    </nav>
+      </nav>
+      <img 
+        src="/images/banner7.png" 
+        alt="Global Network Banner" 
+        className="header-banner" 
+      />
+    </>
   );
 }
