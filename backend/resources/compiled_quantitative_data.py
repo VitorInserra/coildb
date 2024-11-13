ac35b762086ea14253e71e228fcdb3eeea0ebbb4
VitorInserra <inserra.vitor@gmail.com>
2024-11-13 13:00:42 -0500

    Fix CompiledDataResource

--- backend/resources/compiled_quantitative_data.py
+++ backend/resources/compiled_quantitative_data.py
@@ -3,19 +3,21 @@ from sqlalchemy.orm import Session
 from db import get_db
 from models.schemas.compiled_quantitative_data import CompiledDataModel
 from fastapi import APIRouter, Depends
+import json
 # from models.coil_base import CoilBase
 
 class CompiledDataResource:
-    def __init__(self, db: Session):
-        self.router = APIRouter()
+    def __init__(self):
+        self.router = APIRouter(prefix="/compiled-data", tags=["CompiledDataResource"])
 
-        @self.router.get("/compiled-quantitative-data/{faculty_id}", response_model=CompiledDataModel)
+    def get_router(self):
+        @self.router.get("/get-data/{faculty_id}", response_model=CompiledDataModel)
         async def get_compiled_data(compiled_id: int, db: Session = Depends(get_db)):
             return db.query(CompiledQuantitativeData).filter(CompiledQuantitativeData.id == compiled_id).first()
 
-        @self.router.post("/compiled-quantitative-data/", response_model=CompiledDataModel)
+        @self.router.post("/add-data/", response_model=CompiledDataModel)
         async def create_compiled_data(data: CompiledDataModel, db: Session = Depends(get_db)):
-            db_compiled = CompiledQuantitativeData(**dict(data))
+            db_compiled = CompiledQuantitativeData()
             db.add(db_compiled)
             db.commit()
             db.refresh(db_compiled)
