bc202a17fab3dbfc43ff4f1acc07e5ed097d7d9a
Tyler Nguyen <tnguy@ad.unc.edu>
2024-11-16 20:38:36 -0500

    Fixing faculty models to match database

--- backend/models/faculty_recipient.py
+++ backend/models/faculty_recipient.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, Boolean, ARRAY, TIMESTAMP, Float, func
+from sqlalchemy import Column, Integer, String, Boolean, ARRAY, TIMESTAMP, Float, func, Date
 from sqlalchemy.ext.declarative import declarative_base
 from db import engine  # Import engine from db.py
 
@@ -11,35 +11,40 @@ class FacultyRecipient(Base):
     last_name = Column('Last Name', String, nullable=False, primary_key=True, index=False)  
     first_name = Column('First Name', String, nullable=False)  
     email = Column('Email', String, unique=True, nullable=False)  
-    semester_taught = Column('Semester Taught', String)  
+    semester_taught = Column('Semester Taught', String, nullable=False)  
     school = Column('School', String)  
-    department = Column('Department', String)  
-    course_number = Column('Course Number', String) #in database, cnumber was split into name and number
+    department = Column('Department', String)
+    course_name = Column('Course Name', String)  
+    course_number = Column('Course Number', String) 
     sections = Column('Sections', Integer)  
     total_sections_taught = Column('Total # Sections Taught', Integer) 
-    co_taught = Column('Co-taught Semester', String) #missing co-taught year in database  
-    repeat_course = Column('Repeat Course Semester', String) #missing repeat course year
+    course_title = Column('Course Title', String, nullable=False)
+    co_taught_semester = Column('Co-taught Semester', String)
+    co_taught_year = Column('Co-taught Year', Integer)
+    repeat_course_semester = Column('Repeat Course Semester', String) 
+    repeat_course_year = Column('Repeat Course Year', Integer)
     joint_course = Column('Joint - course', String) 
     quarter = Column('Quarter', String)  
     estimated_unc_students = Column('Estimated UNC Students ', Integer) 
     estimated_partner_students = Column('Estimated Partner Students', Integer)  
     unc_students_fdoc_count = Column('Enrolled UNC Students (FDOC)', Integer)  
-
-    #unc_students_ldoc_count = Column(Integer)  
-    #partner_institution1 = Column(String) 
-    #partner_country1 = Column(String)  
-    #partner_faculty1 = Column(String) 
-    #partner_institution2 = Column(String) 
-    #partner_country2 = Column(String)  
-    #partner_faculty2 = Column(String)  
-    #award_date = Column(String)  
-    #graduate_student = Column(String) 
-    #faculty_amount_rewarded = Column(Integer) 
-    #graduate_award_student_amount = Column(Integer) 
-    #repeat_award = Column(Boolean)  
-    #repeat_award_criteria = Column(String)  
-    #coil_champions = Column(String)  
-    #notes = Column(String)  # Additional notes
-    #partner_region = Column(String)
+    unc_students_ldoc_count = Column('Enrolled UNC Students (LDOC)', Integer)  
+    partner_institution1 = Column('Partner Institution 1', String) 
+    partner_country1 = Column('Partner Country 1', String)  
+    partner_faculty1 = Column('Partner Faculty 1', String) 
+    partner_institution2 = Column('Partner Institution 2', String) 
+    partner_country2 = Column('Partner Country 2', String)  
+    partner_faculty2 = Column('Partner Faculty 2', String)
+    credit = Column('EE/HI Credit', String)  
+    award_date = Column('Award Date', Date)  
+    graduate_student = Column('Graduate Student', String) 
+    faculty_amount_rewarded = Column('Faculty Amount Awarded', Integer) 
+    graduate_award_student_amount = Column('Graduate Student Award Amount ', Integer) 
+    repeat_award = Column('Repeat Award ', String)  
+    repeat_award_criteria = Column('Repeat Award Criteria ', String)  
+    coil_champions = Column('COIL Champion', String)  
+    notes = Column('Notes', String)  # Additional notes
+    partner_region = Column('Partner region', String)
+    cancelled = Column('Cancelled', Boolean)
 
 Base.metadata.create_all(bind=engine)
--- backend/models/schemas/faculty_recipient.py
+++ backend/models/schemas/faculty_recipient.py
@@ -1,5 +1,6 @@
 from pydantic import BaseModel
 from typing import Optional
+from datetime import date
 
 class FacultyRecipientModel(BaseModel):
     last_name:  Optional[str] = None
@@ -8,32 +9,38 @@ class FacultyRecipientModel(BaseModel):
     semester_taught:  Optional[str] = None
     school:  Optional[str] = None
     department:  Optional[str] = None
+    course_name: Optional[str] = None
     course_number:  Optional[str] = None
     sections: Optional[str] = None
     total_sections_taught: Optional[int] = None
-    co_taught: Optional[str] = None  # Allow None values
-    repeat_course: Optional[str] = None  # Allow None values
+    course_title: Optional[str] = None
+    co_taught_semester: Optional[str] = None  # Allow None values
+    co_taught_year: Optional[int] = None
+    repeat_course_semester: Optional[str] = None  # Allow None values
+    repeat_course_year: Optional[int] = None
     joint_course: Optional[str] = None  # Allow None values
     quarter: Optional[str] = None  # Allow None values
     estimated_unc_students: Optional[int] = None
     estimated_partner_students: Optional[int] = None
     unc_students_fdoc_count: Optional[int] = None
-    #unc_students_ldoc_count: int
-    #partner_institution1: str
-    #partner_country1: str
-    #partner_faculty1: str
-    #partner_institution2: str
-    #partner_country2: str
-    #partner_faculty2: str
-    #award_date: str 
-    #graduate_student: str
-    #faculty_amount_rewarded: int
-    #graduate_award_student_amount: int
-    #repeat_award: bool
-    #repeat_award_criteria: str
-    #coil_champions: str
-    #notes: str
-    #partner_region: str
+    unc_students_ldoc_count: Optional[int] = None
+    partner_institution1: Optional[str] = None
+    partner_country1: Optional[str] = None
+    partner_faculty1: Optional[str] = None
+    partner_institution2: Optional[str] = None
+    partner_country2: Optional[str] = None
+    partner_faculty2: Optional[str] = None
+    credit: Optional[str] = None
+    award_date: Optional[date] = None
+    graduate_student: Optional[str] = None
+    faculty_amount_rewarded: Optional[int] = None
+    graduate_award_student_amount: Optional[int] = None
+    repeat_award: Optional[str] = None
+    repeat_award_criteria: Optional[str] = None
+    coil_champions: Optional[str] = None
+    notes: Optional[str] = None
+    partner_region: Optional[str] = None
+    cancelled: Optional[bool] = None
     class Config:
         orm_mode = True
         from_attributes = True
