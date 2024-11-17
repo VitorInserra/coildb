1aa2ec8dc86764d856d0d01eea496a9d5ac4a424
Tyler Nguyen <tnguy@ad.unc.edu>
2024-11-16 19:06:09 -0500

    Added a get-recipient filter

--- backend/resources/faculty_recipient.py
+++ backend/resources/faculty_recipient.py
@@ -4,7 +4,7 @@ from db import SessionLocal
 from db import get_db
 from models.schemas.faculty_recipient import FacultyRecipientModel
 from fastapi import APIRouter, Depends
-from typing import List
+from typing import List, Union
 # from models.coil_base import CoilBase
 
 class FacultyRecipientResource:
@@ -17,9 +17,14 @@ class FacultyRecipientResource:
             faculty_list = db.query(FacultyRecipient)
             return [FacultyRecipientModel.from_orm(faculty) for faculty in faculty_list]
 
-        @self.router.get("/get-recipient/{faculty_id}", response_model=FacultyRecipientModel)
-        async def get_faculty_recipient(faculty_id: int, db: Session = Depends(get_db)):
-            return db.query(FacultyRecipient).filter(FacultyRecipient.id == faculty_id).first()
+        @self.router.get("/get-recipient/{column}/{input}/", response_model=Union[FacultyRecipientModel, List[FacultyRecipientModel]])
+        async def get_faculty_recipient(column: str, input: str, db: Session = Depends(get_db)):
+            query = db.query(FacultyRecipient).filter(getattr(FacultyRecipient, column) == input)
+            result = query.all()
+            if result:
+                return result
+            else:
+                return {"message": "No matching faculty recipient found"}
 
         @self.router.post("/post-recipient/", response_model=FacultyRecipientModel)
         async def create_faculty_recipient(faculty: FacultyRecipientModel, db: Session = Depends(get_db)):
