8831ac4a33f8ffe847bb279302d15a85c4c28d55
VitorInserra <inserra.vitor@gmail.com>
2024-12-01 17:59:49 -0500

    Edit DB env setters

--- backend/db.py
+++ backend/db.py
@@ -5,11 +5,11 @@ import os
 
 load_dotenv(os.getcwd() + '/.env')
 
-DB_USER = os.getenv("DEV_DB_USERNAME")
-DB_PASSWORD = os.getenv("DEV_DB_PASSWORD")
-DB_HOST = os.getenv("DEV_DB_HOST")
-DB_PORT = os.getenv("DEV_DB_PORT")
-DB_NAME = os.getenv("DEV_DB_NAME")
+DB_USER = os.getenv("DB_USERNAME")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
 
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 print(DATABASE_URL)
