17c41af3ae9f90eba1ad138c5e2ee71b2da6dcee
Pallavi Sastry <89313238+pallavi-sastry@users.noreply.github.com>
2024-12-01 16:13:01 -0500

    frontend tables connection plus key statistics bar (#9)

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from resources.coil_base import CoilBase
 from resources.compiled_quantitative_data import CompiledDataResource
 from resources.faculty_recipient import FacultyRecipientResource
@@ -8,6 +9,15 @@ from resources.gradstudent_recipient import GradStudentRecipientResource
 import uvicorn
 
 app = FastAPI()
+
+# Add CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3001"],  # Allow requests from your frontend origin
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 app.include_router(CoilBase().get_router())
 app.include_router(CompiledDataResource().get_router())
