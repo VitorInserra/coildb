0ee3633dcacf6999603bb9f6233da7a75d5446f3
VitorInserra <inserra.vitor@gmail.com>
2024-12-01 18:03:49 -0500

    Fix hostname for prod deploy

--- backend/prod.py
+++ backend/prod.py
@@ -26,4 +26,4 @@ app.include_router(GradStudentRecipientResource().get_router())
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8080, reload=True)
+    uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=True)
