a0ccb083ed8ca5ef1df13c140829a9c520097fd2
VitorInserra <inserra.vitor@gmail.com>
2024-12-06 15:45:22 -0500

    Fix cross origin

--- backend/prod.py
+++ backend/prod.py
@@ -5,7 +5,7 @@ from api import app
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://0.0.0.0:80"],
+    allow_origins=["http://coildb-frontend-dept-coildb.apps.cloudapps.unc.edu"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -13,4 +13,4 @@ app.add_middleware(
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host='0.0.0.0', port=8080)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
