54c3b37b802a8852752a9eb89bc4b048d70b66ce
VitorInserra <inserra.vitor@gmail.com>
2024-12-06 23:52:03 -0500

    Add router options

--- backend/resources/coil_base.py
+++ backend/resources/coil_base.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, Response, Request, middleware
+from fastapi.responses import JSONResponse, Response
 from sqlalchemy.orm import Session
 from sqlalchemy import text
 from db import get_db
@@ -12,9 +13,13 @@ class CoilBase:
     def get_router(self):
 
         @self.router.post("/ping")
-        async def ping(
-            username: str = Depends(get_current_username)
-        ):
+        async def ping(username: str = Depends(get_current_username)):
             return {"message": "ok"}
+
+        @self.router.options("/")
+        async def options_root():
+            allowed_methods = ["GET", "OPTIONS"]
+            headers = {"Allow": ", ".join(allowed_methods)}
+            return JSONResponse(content=None, headers=headers)
 
         return self.router
