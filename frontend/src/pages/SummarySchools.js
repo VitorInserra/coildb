f91e8a3ee8abdf2fd0646a7bb5d2ae5efad7fa9f
Pallavi Sastry <89313238+pallavi-sastry@users.noreply.github.com>
2024-12-08 12:25:20 -0500

    commented out repeat faculty and unique faculty

--- frontend/src/pages/SummarySchools.js
+++ frontend/src/pages/SummarySchools.js
@@ -5,8 +5,8 @@ export default function SummarySchools({ addStarredReport }) {
   const columnDefs = [
     { headerName: "School", field: "school", filter: true, editable: true },
     { headerName: "School Count", field: "school_count", filter: true, editable: true },
-    { headerName: "Repeat Faculty", field: "repeat_faculty", filter: true, editable: true },
-    { headerName: "Unique Faculty", field: "unique_faculty", filter: true, editable: true },
+    //{ headerName: "Repeat Faculty", field: "repeat_faculty", filter: true, editable: true },
+    //{ headerName: "Unique Faculty", field: "unique_faculty", filter: true, editable: true },
   ];
 
   return (
